ffa4db89eff52bec2af987f05a9e7ad9bd88e6f1
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-21 17:26:52 +0800

    Add squares and style them
    Add squares with js.
    Insert class and style.
    Style with class.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+(function board() {
+  const boardElement = document.querySelector("#board");
+  for (let i = 1; i <= 9; i++) {
+    const boardSquare = document.createElement("div");
+    boardSquare.setAttribute("id", `square-${i}`);
+    boardSquare.classList.add("board-square");
+    boardElement.appendChild(boardSquare);
+  }
+})();
