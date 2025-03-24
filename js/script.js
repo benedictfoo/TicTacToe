ecd685d3b481ab0700dd53d0ecae49f5a0faaaa2
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-24 10:45:38 +0800

    Game Checks winner.
    Check against a list of win conditions

--- js/script.js
+++ js/script.js
@@ -21,6 +21,46 @@
         turnCount++;
         target.dataset.value = turnCount % 2 === 1 ? "X" : "O";
       }
+      checkIfWon();
     }
   });
+  // Array of winning
+  const winArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+  // function to check if won
+  function checkIfWon() {
+    // winArray.forEach(condition=>{
+    //   if()
+    // })
+    const squares = document.querySelectorAll(".board-square");
+    function getSquareValue(oneIndex) {
+      return squares[oneIndex - 1].dataset.value;
+    }
+    for (let i = 0; i < winArray.length; i++) {
+      const condition = winArray[i];
+      const first = getSquareValue(condition[0]);
+      const second = getSquareValue(condition[1]);
+      const third = getSquareValue(condition[2]);
+      if (
+        ["X", "O"].includes(first) &&
+        ["X", "O"].includes(second) &&
+        ["X", "O"].includes(third) &&
+        first === second &&
+        first === third
+      ) {
+        return `Player ${first === "X" ? "1" : "2"} won!`;
+      }
+      if (turnCount === 9) {
+        return "There is no winner...";
+      }
+    }
+  }
 })();
