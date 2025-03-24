6ad01ed89a2975a6515faa4ee08f423171426f3a
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-24 08:25:56 +0800

    Make game Object with square click event
    Click event sets a data attribute
    Object has turn variable which changes each accepted click

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-(function board() {
+(function () {
+  // Make Board
   const boardElement = document.querySelector("#board");
   for (let i = 1; i <= 9; i++) {
     const boardSquare = document.createElement("div");
     boardSquare.setAttribute("id", `square-${i}`);
     boardSquare.classList.add("board-square");
+    boardSquare.dataset.value = "";
     boardElement.appendChild(boardSquare);
   }
+})();
+(function () {
+  let turn = "X";
+  // Click Event
+  const boardElement = document.querySelector("#board");
+  boardElement.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.classList.contains("board-square")) {
+      if (target.dataset.value === "") {
+        target.dataset.value = turn;
+        turn = turn === "X" ? "O" : "X";
+      }
+    }
+  });
 })();
